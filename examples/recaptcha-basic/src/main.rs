99ef112c39a1ab0d11a0e1d2bf11aa3d1eef288c
Dario Lencina <dario@securityunion.dev>
2022-08-13 06:50:47 -0400

    save

--- examples/recaptcha-basic/src/main.rs
+++ examples/recaptcha-basic/src/main.rs
@@ -5,11 +5,19 @@ use yew_recaptcha_v3::recaptcha::Recaptcha;
 #[function_component(App)]
 fn app_component() -> Html {
     let recaptcha_ref = NodeRef::default();
-    let on_click = Callback::from(|_| {
+    let on_execute = Box::new(use_state(|| None));
+    let on_execute_copy = on_execute.clone();
+    let on_click = Callback::from(move |_| {
         log!("click");
+        let on_execute_copy = on_execute.clone();
+        on_execute.set(Some(Callback::from( move |w: String| {
+            log!("on execute");
+            on_execute_copy.set(None);
+            ()
+        })));
         ()
     });
-
+    let on_execute_value = &**on_execute_copy;
     html! {
         <>
             <button onclick={on_click}>
@@ -17,7 +25,7 @@ fn app_component() -> Html {
             </button>
             <Recaptcha
             site_key="6LeH_x8UAAAAAKKuaaod4GsENkTJTHdeQIm8l6y2"
-            // onResolved={onResolved}
+            on_execute={on_execute_value}
             ref={recaptcha_ref}/>
         </>
     }
--- src/recaptcha/mod.rs
+++ src/recaptcha/mod.rs
@@ -1,29 +1,65 @@
+use gloo_console::log;
+use gloo_utils::document;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::AddEventListenerOptions;
 use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
 pub struct RecaptchaProps {
     pub site_key: String,
+    pub on_execute: Option<Callback<String>>,
 }
 
 // Docs: https://developers.google.com/recaptcha/docs/v3
-pub struct Recaptcha;
+#[function_component(Recaptcha)]
+pub fn recaptcha_component(props: &RecaptchaProps) -> Html {
+    log!("cause child re-render");
+    use_effect_with_deps(
+        move |_| {
+            log!("configuring dom");
 
-impl Component for Recaptcha {
-    type Message = ();
-    type Properties = RecaptchaProps;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
+            || ()
+        },
+        (), // dependents
+    );
+    if let Some(callback) = &props.on_execute {
+        log!("calling callback");
+        callback.emit("balls".to_string());
+    } else {
+        log!("no callback");
     }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            {
-                format!(
-                    "site_key: {}",
-                    ctx.props().site_key,
-                )
-            }
-        }
+    html! {
+        <>
+        </>
     }
+}
+
+fn injectScript(locale: String) -> Result<(), JsValue> {
+    // window.GoogleRecaptchaLoaded = () => {
+    //   while (renderers.length) {
+    //     const renderer = renderers.shift();
+    //     renderer?.();
+    //   }
+    // };
+
+    let script = document().create_element("script").unwrap();
+    script.set_attribute("async", "true")?;
+    script.set_id("recaptcha");
+    let listener = Closure::wrap(Box::new(|_| {}) as Box<dyn FnMut(JsValue)>);
+    let options = AddEventListenerOptions::new();
+    script.add_event_listener_with_callback_and_add_event_listener_options(
+        "onerror",
+        listener.as_ref().unchecked_ref(),
+        &options,
+    )?;
+    script.set_attribute(
+        "src",
+        "https://www.google.com/recaptcha/api.js?onload=GoogleRecaptchaLoaded&render=explicit",
+    )?;
+
+    script.set_attribute("type", "text/javascript")?;
+
+    // nonce && script.setAttribute("nonce", nonce);
+    document().body().unwrap().append_child(&script)?;
+    Ok(())
 }
